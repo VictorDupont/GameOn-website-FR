1c623a7ef4b875d56d2cb77eb6757f28a5e3dae5
Supernova <dpntvictor@gmail.com>
2022-01-31 14:52:42 +0100

    GameOn!

---
+++ modal.js
@@ -0,0 +1,28 @@
+function editNav() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.getElementById("close-modal");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+modalCloseBtn.addEventListener("click", closeModal);
+
+// launch modal form
+function launchModal() {
+	modalbg.style.display = "block";
+}
+
+function closeModal() {
+	modalbg.style.display = "none";
+}
