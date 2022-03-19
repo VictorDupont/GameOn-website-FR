9202dd3419f8c8b13ae4ccb6eb7a2a59db8a7b8f
Supernova <dpntvictor@gmail.com>
2022-03-19 16:57:59 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
+// Regex formats
+
 const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 const birthdateRegex = /^(19|20)\d{2}[-](0?[1-9]|1[012])[-](0[1-9]|[12]\d|3[01])$/;
 const quantityRegex = /^\+?(0|[1-9]\d*)$/;
+
+// Check functions - Check regex format, lenght input and if radio or checkbox is checked
 
 function RegexFormat(string, regex) {
 	return regex.test(string);
@@ -15,13 +19,16 @@ function isRadioChecked() {
 }
 
 function isCheckboxChecked(id) {
-	return document.getElementById(id).checked;
+	// console.log(document.getElementById(id).checked);
+	return document.getElementById(String(id)).checked;
 }
 
 const form = document.getElementById("reservation");
 const successCloseBtn = document.getElementById("success-close-btn");
 
 form.addEventListener("submit", validation);
+
+// DOM elements - Get all of these to interact
 
 const firstname = document.getElementById("firstname");
 const lastname = document.getElementById("lastname");
@@ -38,53 +45,76 @@ const birthdateMessage = document.getElementById("data-birthdate");
 const quantityMessage = document.getElementById("data-quantity");
 const cityMessage = document.getElementById("data-city");
 const cguMessage = document.getElementById("data-cgu");
+const formMessage = document.getElementById("data-form");
+
+// All check functions for the form inputs
+
+// Example with firstnameCheck function
+// Declare a variable with function which checks the lenght of the firstname input, which must be 2 characters at least
+// If the check function return false, the error message is displayed
+// The function return the variable output (useful for the main function at the bottom)
 
 function firstnameCheck() {
-	let firstnameValidation = lengthCheck(firstname.value.length, 2);
+	let firstnameValidation = lengthCheck(firstname.value.trim().length, 2);
 	if (!firstnameValidation) firstnameMessage.setAttribute("data-error-visible", true);
+	else firstnameMessage.setAttribute("data-error-visible", false);
 	return firstnameValidation;
 }
 
 function lastnameCheck() {
 	let lastnameValidation = lengthCheck(lastname.value.length, 2);
 	if (!lastnameValidation) lastnameMessage.setAttribute("data-error-visible", true);
+	else lastnameMessage.setAttribute("data-error-visible", false);
 	return lastnameValidation;
 }
 
 function emailCheck() {
 	let emailValidation = RegexFormat(email.value, emailRegex);
 	if (!emailValidation) emailMessage.setAttribute("data-error-visible", true);
+	else emailMessage.setAttribute("data-error-visible", false);
 	return emailValidation;
 }
 
 function birthdateCheck() {
 	let birthdateValidation = RegexFormat(birthdate.value, birthdateRegex);
 	if (!birthdateValidation) birthdateMessage.setAttribute("data-error-visible", true);
+	else birthdateMessage.setAttribute("data-error-visible", false);
 	return birthdateValidation;
 }
 
 function quantityCheck() {
 	let quantityValidation = RegexFormat(quantity.value, quantityRegex);
 	if (!quantityValidation) quantityMessage.setAttribute("data-error-visible", true);
+	else quantityMessage.setAttribute("data-error-visible", false);
 	return quantityValidation;
 }
 
 function cityCheck() {
 	let cityValidation = isRadioChecked();
 	if (!cityValidation) cityMessage.setAttribute("data-error-visible", true);
+	else cityMessage.setAttribute("data-error-visible", false);
 	return cityValidation;
 }
 
 function cguCheck() {
 	let cguValidation = isCheckboxChecked("cgu");
 	if (!cguValidation) cguMessage.setAttribute("data-error-visible", true);
+	else cguMessage.setAttribute("data-error-visible", false);
 	return cguValidation;
 }
+
+// Remove the form from the user visibility and display a success message to him
 
 function formValided() {
 	form.style.display = "none";
 	successMessage.style.display = "block";
+	let buttonAbsolute = document.getElementById("btnsubmit");
+	buttonAbsolute.classList.add("absolute");
 }
+
+// Main function, blocks the refresh initial action of the form.
+// Checks all inputs with their function
+// When all of these are valided, the formValided function is called and the close button is set to hidden.
 
 function validation(event) {
 	event.preventDefault();
@@ -105,6 +135,11 @@ function validation(event) {
 		cityValide &&
 		cguValide;
 
-	if (valide) formValided();
-	modalCloseBtn.style.display = "none";
+	if (valide) {
+		formValided();
+		modalCloseBtn.style.display = "none";
+		document.getElementById("reservation").reset();
+	} else {
+		formMessage.setAttribute("data-error-visible", true);
+	}
 }
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -13,16 +13,26 @@ const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.getElementById("close-modal");
 const successMessage = document.getElementById("success-message");
 const successClose = document.getElementById("success-close");
+const successCloseCross = document.getElementById("close-modal-success");
 const body = document.querySelector("body");
 
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalCloseBtn.addEventListener("click", closeModal);
 successClose.addEventListener("click", closeSuccess);
+successCloseCross.addEventListener("click", closeSuccess);
 
 function launchModal() {
 	modalbg.style.display = "block";
 	modalCloseBtn.style.display = "block";
 	successMessage.style.display = "none";
+	formMessage.setAttribute("data-error-visible", false);
+	firstnameMessage.setAttribute("data-error-visible", false);
+	lastnameMessage.setAttribute("data-error-visible", false);
+	emailMessage.setAttribute("data-error-visible", false);
+	birthdateMessage.setAttribute("data-error-visible", false);
+	quantityMessage.setAttribute("data-error-visible", false);
+	cityMessage.setAttribute("data-error-visible", false);
+	cguMessage.setAttribute("data-error-visible", false);
 }
 
 function closeModal() {
